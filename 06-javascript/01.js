68dffafddf8ac99a80f9c2916461126f340810a3
Leonardo-A-Pacheco <l00000111.pacheco@gmail.com>
2023-12-03 19:05:06 -0300

    adicionando script.js funcao toggle

---
+++ 06-javascript/01.js
@@ -0,0 +1,13 @@
+alert("banana")
+
+// let declarar e atribuir valor
+
+let v1 = 'banana é uma fruta bonita'
+
+alert(v1)
+
+// const não pode mudar o valor
+
+const SerHumano = true;
+
+SerHumano = false;
